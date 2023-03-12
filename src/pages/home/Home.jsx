ecb9c53178c7860a02e63b7dc0a8c24116611b37
officialYash <38553961+officialYash@users.noreply.github.com>
2023-03-12 18:36:33 +0530

    Slider infinite install and set a slide components and create a design content on scss

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import Slide from '../../components/Slide/Slide'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
 import "./Home.scss"
+import { cards } from '../../data'
+import CatCard from '../../components/catCard/CatCard'
 const Home = () => {
   return (
     <div className='home'>
     <Featured/>
     <TrustedBy/>
+    <Slide slidesToShow={5} arrowScroll={5}>
+    {cards.map(card =>(
+    <CatCard  item={card} key={card.id} />
+))}
+    </Slide>
     </div>
   )
 }
